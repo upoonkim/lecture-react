c536b5101574ce8efab526d1845f25f603fa2303
kim.sanghoon <kim.sanghoon@lotte.net>
2022-05-25 18:01:46 +0900

    #7 2022 05 25 상태를 갱신하기

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,174 +1,53 @@
-import { formatRelativeDate } from "./js/helpers.js";
-import store from "./js/Store.js";
+// 리액트는 하나의 엘레멘트 단위이기 때문에 <> </> 와 같이 전체를 한번 감싸줘야 한다. 동일 레벨의 여러개 엘리멘트가 존재하면 안된다. 리액트는 루트 노드가 있어야 한다.
+// 엘리멘트보다 더 큰 개념이 컴포넌트. 컴포넌트는 UI를 나타내는 엘리먼트와 어플리케이션 로직을 포함한 상위 개념이다.
 
-const TabType = {
-  KEYWORD: "KEYWORD",
-  HISTORY: "HISTORY",
-};
-
-const TabLabel = {
-  [TabType.KEYWORD]: "추천 검색어",
-  [TabType.HISTORY]: "최근 검색어",
-};
-
+// 리액트 자채에 있는 컴포넌트를 상속
 class App extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      searchKeyword: "",
-      searchResult: [],
-      submitted: false,
-      selectedTab: TabType.KEYWORD,
-      keywordList: [],
-      historyList: [],
-    };
-  }
+    constructor(){
+        super();
 
-  componentDidMount() {
-    const keywordList = store.getKeywordList();
-    const historyList = store.getHistoryList();
-
-    this.setState({
-      keywordList,
-      historyList,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.search(this.state.searchKeyword);
-  }
-
-  search(searchKeyword) {
-    const searchResult = store.search(searchKeyword);
-    const historyList = store.getHistoryList();
-
-    this.setState({
-      searchKeyword,
-      searchResult,
-      historyList,
-      submitted: true,
-    });
-  }
-
-  handleReset() {
-    this.setState({
-      searchKeyword: "",
-      searchResult: [],
-      submitted: false,
-    });
-  }
-
-  handleChangeInput(event) {
-    const searchKeyword = event.target.value;
-
-    if (searchKeyword.length <= 0 && this.state.submitted) {
-      return this.handleReset();
+        this.state = {
+            searchKeyword: 'qweqwe',
+        }
     }
 
-    this.setState({ searchKeyword });
-  }
+    handleChangeInput(event){
+        
+    }
 
-  handleClickRemoveHistory(event, keyword) {
-    event.stopPropagation();
-
-    store.removeHistory(keyword);
-    const historyList = store.getHistoryList();
-    this.setState({ historyList });
-  }
-
-  render() {
-    const searchForm = (
-      <form
-        onSubmit={(event) => this.handleSubmit(event)}
-        onReset={() => this.handleReset()}
-      >
-        <input
-          type="text"
-          placeholder="검색어를 입력하세요"
-          autoFocus
-          value={this.state.searchKeyword}
-          onChange={(event) => this.handleChangeInput(event)}
-        />
-        {this.state.searchKeyword.length > 0 && (
-          <button type="reset" className="btn-reset"></button>
-        )}
-      </form>
-    );
-
-    const searchResult =
-      this.state.searchResult.length > 0 ? (
-        <ul className="result">
-          {this.state.searchResult.map(({ id, imageUrl, name }) => (
-            <li key={id}>
-              <img src={imageUrl} />
-              <p>{name}</p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty-box">검색 결과가 없습니다</div>
-      );
-
-    const keywordList = (
-      <ul className="list">
-        {this.state.keywordList.map(({ id, keyword }, index) => (
-          <li key={id} onClick={() => this.search(keyword)}>
-            <span className="number">{index + 1}</span>
-            <span>{keyword}</span>
-          </li>
-        ))}
-      </ul>
-    );
-
-    const historyList = (
-      <ul className="list">
-        {this.state.historyList.map(({ id, keyword, date }) => (
-          <li key={id} onClick={() => this.search(keyword)}>
-            <span>{keyword}</span>
-            <span className="date">{formatRelativeDate(date)}</span>
-            <button
-              className="btn-remove"
-              onClick={(event) => this.handleClickRemoveHistory(event, keyword)}
-            />
-          </li>
-        ))}
-      </ul>
-    );
-
-    const tabs = (
-      <>
-        <ul className="tabs">
-          {Object.values(TabType).map((tabType) => (
-            <li
-              key={tabType}
-              className={this.state.selectedTab === tabType ? "active" : ""}
-              onClick={() => this.setState({ selectedTab: tabType })}
-            >
-              {TabLabel[tabType]}
-            </li>
-          ))}
-        </ul>
-        {this.state.selectedTab === TabType.KEYWORD && keywordList}
-        {this.state.selectedTab === TabType.HISTORY && historyList}
-      </>
-    );
-
-    return (
-      <>
-        <header>
-          <h2 className="container">검색</h2>
-        </header>
-        <div className="container">
-          {searchForm}
-          <div className="content">
-            {this.state.submitted ? searchResult : tabs}
-          </div>
-        </div>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <header>
+                    <h2 className="container">검색</h2>
+                </header>
+                <div className="container">
+                    <form id="search-form-view">
+                        <input type="text" placeholder="검색어를 입력하시오." autoFocus value={this.state.searchKeyword} onChange={event=>this.handleChangeInput(event)}/>
+                        <button type="reset" className="btn-reset"></button>
+                    </form>
+                </div>
+            </>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#app"));
+
+
+// const element = (                  // 엘리먼트 예시
+//     <> 
+//     <header>
+//         <h2 className="container">검색</h2>
+//     </header>
+//     <div className="container">
+//     <form id="search-form-view">
+//         <input type="text" placeholder="검색어를 입력하시오." autoFocus />
+//         <button type="reset" className="btn-reset"></button>
+//     </form>
+// </div>
+// </>
+// );
+// JSX 는 class가 className이다. 
+// ReactDOM.render(element, document.querySelector("#app"));
